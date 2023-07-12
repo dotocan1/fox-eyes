d324033f5e4036620c0b9b123034e9f39445d25f
Dominik Otočan <49837535+dotocan1@users.noreply.github.com>
2023-07-12 09:03:30 +0200

    made some small changes and added an icon

--- scripts.js
+++ scripts.js
@@ -4,12 +4,14 @@ function displayNotification () {
     if (Notification.permission === "granted") {
         var notification = new Notification("Hello!", {
             body: "Rest your eyes!",
+            icon: "./fox-eyes.png"
         });
 
         // TODO: add sound
         // Play sound
         // var audio = new Audio("path_to_sound_file.mp3");
         // audio.play();
+        timerRunning = false;
     }
 }
 
@@ -30,7 +32,7 @@ function startTimer (duration, display) {
 
         if (timer <= 0) {
             worker.terminate();
-            display.textContent = "Timer finished!";
+            display.textContent = "Rest your eyes then start the timer again!";
         }
     };
 
@@ -41,24 +43,30 @@ function startTimer (duration, display) {
 
 // Display the timer in the DOM
 var display = document.getElementById("timer");
-
+let timerRunning = false;
 
 //
 let button = document.querySelector(".button");
 // Set the timer duration to 20 minutes (in seconds)
 var twentyMinutes = 20 * 60;
-
 button.addEventListener('click', () => {
+    // if the timer is already runnign
+    if(timerRunning){
+        alert("Timer is already running!")
+        console.log('hey')
+    }
     // Check if the browser supports notifications
-    if ("Notification" in window) {
+    else if ("Notification" in window) {
+        console.log('hey1')
         // Request permission to display notifications
         Notification.requestPermission().then(function (permission) {
             if (permission === "granted") {
                 // Schedule the first notification
                 // displayNotification();
 
+                timerRunning = true;
                 // Schedule subsequent notifications every 20 minutes
-                setTimeout(displayNotification, 20 * 60 * 1000);
+                // setTimeout(displayNotification, 20 * 60 * 1000);
                 startTimer(twentyMinutes, display);
             }
         });
